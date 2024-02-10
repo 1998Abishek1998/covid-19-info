9f1119da1b9c11ff5091b9dfc1b14a3a56f8e136
Abishek Timsina <uzamakiu@gmail.com>
2024-02-10 13:26:32 +0545

    engine updates

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -6,101 +6,103 @@ import numeral from "numeral"
 import casesTypesColors from "./util"
 
 const options = {
-	legend: {
-		display: false,
-	},
-	elements: {
-		point: {
-			radius: 0,
-		},
-	},
-	maintainaspectRatio: false,
-	tooltips: {
-		mode: "index",
-		intersect: false,
-		callbacks: {
-			label: function (tooltipItem, data) {
-				return numeral(tooltipItem.value).format("+0,0")
-			},
-		},
-	},
-	scales: {
-		xAxes: [
-			{
-				type: "time",
-				time: {
-					format: "MM/DD/YY",
-					tooltipFormat: "ll",
-				},
-			},
-		],
-		yAxes: [
-			{
-				gridLines: {
-					display: false,
-				},
-				ticks: {
-					//include dollar sign in the ticks
-					callback: function (value, index, values) {
-						return numeral(value).format("0a")
-					},
-				},
-			},
-		],
-	},
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainaspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0")
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          //include dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format("0a")
+          },
+        },
+      },
+    ],
+  },
 }
 
 function LineGraph({ caseType }) {
-	const [data, setData] = useState({})
+  const [data, setData] = useState({})
 
-	const buildChartData = (data, casesType) => {
-		const chartData = []
-		let lastDataPoint
-		for (let date in data.cases) {
-			if (lastDataPoint) {
-				const newDataPoint = {
-					x: date,
-					y: data[casesType][date] - lastDataPoint,
-				}
-				chartData.push(newDataPoint)
-			}
-			lastDataPoint = data[casesType][date]
-		}
-		return chartData
-	}
+  const buildChartData = (data, casesType) => {
+    const chartData = []
+    let lastDataPoint
+    for (let date in data.cases) {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        }
+        chartData.push(newDataPoint)
+      }
+      lastDataPoint = data[casesType][date]
+    }
+    return chartData
+  }
 
-	useEffect(() => {
-		const fetchData = async () => {
-			await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-				.then((response) => response.json())
-				.then((data) => {
-					const chartData = buildChartData(data, caseType)
-					setData(chartData)
-				})
-		}
-		fetchData()
-	}, [])
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          const chartData = buildChartData(data, caseType)
+          setData(chartData)
+        })
+    }
+    fetchData()
 
-	return (
-		<div className="linegraph">
-			<h1>Worldwide {caseType} </h1>
-			{data?.length > 0 && (
-				<Line
-					className="linegraph__actual"
-					options={options}
-					data={{
-						datasets: [
-							{
-								data: data,
-								backgroundColor: casesTypesColors[caseType].hex,
-								borderColor: casesTypesColors[caseType].hex,
-							},
-						],
-					}}
-				/>
-			)}
-		</div>
-	)
+    return () => fetchData()
+  }, [caseType])
+
+  return (
+    <div className="linegraph">
+      <h1>Worldwide {caseType} </h1>
+      {data?.length > 0 && (
+        <Line
+          className="linegraph__actual"
+          options={options}
+          data={{
+            datasets: [
+              {
+                data: data,
+                backgroundColor: casesTypesColors[caseType].hex,
+                borderColor: casesTypesColors[caseType].hex,
+              },
+            ],
+          }}
+        />
+      )}
+    </div>
+  )
 }
 
 export default LineGraph
